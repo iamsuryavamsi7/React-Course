8b421aa0f209a55b4429bc43d2e425913f216ec1
Surya Vamsi <iamsuryavamsi@gmail.com>
2024-05-07 22:49:00 +0530

    Added 09Project

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Card from './components/Card'
+import ThemeBtn from './components/ThemeBtn'
+
+
+function App() {
+
+  return (
+	<>
+
+		<ThemeBtn />
+
+		<Card />
+			
+	</>
+  )
+}
+
+export default App
